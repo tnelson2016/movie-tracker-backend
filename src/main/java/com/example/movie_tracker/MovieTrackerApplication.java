b34290e4a25963c8b0a3abf6d12b63243fb7d3eb
Troy Nelson <troynelson@Troys-MBP.attlocal.net>
2025-12-25 14:18:17 -0500

    Fix package scanning so controllers load

--- src/main/java/com/example/movie_tracker/MovieTrackerApplication.java
+++ src/main/java/com/example/movie_tracker/MovieTrackerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieTrackerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MovieTrackerApplication.class, args);
 	}
-
 }
