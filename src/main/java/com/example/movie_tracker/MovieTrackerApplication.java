6dd6825a188043eb9713e820dbbeb1665c9f1537
Troy Nelson <troynelson@Troys-MBP.attlocal.net>
2025-12-25 14:32:22 -0500

    Fix controller package scanning

--- src/main/java/com/example/movie_tracker/MovieTrackerApplication.java
+++ src/main/java/com/example/movie_tracker/MovieTrackerApplication.java
@@ -3,9 +3,10 @@ package com.example.movie_tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.movie_tracker")
 public class MovieTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieTrackerApplication.class, args);
 	}
 }
+
